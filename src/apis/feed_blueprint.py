79ad3920d723920b5f794fe7b2c1c2f08859ea17
thanhtruongtran <thanhtruongtran23@gmail.com>
2025-04-12 08:11:15 +0700

    update api

--- src/apis/feed_blueprint.py
+++ src/apis/feed_blueprint.py
@@ -2,6 +2,7 @@ from sanic import Blueprint, json
 from sanic_ext import openapi
 
 from src.services.feed.get_feed import GetFeed
+from src.services.feed.interest_feed import GetInterestFeed
 from src.utils.logger import get_logger
 
 logger = get_logger("Feed Blueprint")
@@ -18,3 +19,30 @@ async def get_feed(request):
     feeds = GetFeed(user_id).get_feed()
 
     return json(feeds)
+
+
+@bp.get("/feed/interest")
+@openapi.tag("Feed")
+@openapi.summary("Get Interest Feed")
+@openapi.parameter("user_id", str, "query", required=True)
+@openapi.secured("Authorization")
+async def get_interest_feed(request):
+    user_id = request.args.get("user_id")
+    feeds = GetInterestFeed(user_id).get_interest_feed()
+    feeds = sorted(feeds, key=lambda x: x["lastUpdated"], reverse=True)
+
+    return json(feeds)
+
+
+@bp.get("/feed/all")
+@openapi.tag("Feed")
+@openapi.summary("Get All Feed")
+@openapi.parameter("user_id", str, "query", required=True)
+@openapi.secured("Authorization")
+async def get_all_feed(request):
+    user_id = request.args.get("user_id")
+    feed_v0 = GetFeed(user_id).get_feed()
+    feed_v1 = GetInterestFeed(user_id).get_interest_feed()
+
+    all_feed = feed_v0 + feed_v1
+    return json(all_feed)
